0eff8574b0cf11a4e65dcaeedf0e649ea922f66b
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-08-03 16:30:35 -0600

    Add quotes data. Add display of today's NEO

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import './App.css'
 import React, { useEffect, useState } from "react"
 import dayjs from 'dayjs'
-import data from "./data.js"
+// import data from "./data.js"
+import quotes from "./quotes"
 
 const App = () => {
-  console.log(data)
+  console.log(quotes)
   const date = dayjs().format("YYYY-MM-DD")
   const [userFormattedDate, setUserFormattedDate] = useState(dayjs(date).format("MM/DD/YYYY"))
   const [startDate, setStartDate] = useState(date)
   const [endDate, setEndDate] = useState(dayjs(date).subtract(6, "day").format("YYYY-MM-DD"))
 
-  const [NASAData, setNASAData] = useState(data)
-  console.log(data)
+// NASA DATA & OBJECT KEYS
+  const [NASAData, setNASAData] = useState("")
+  const [dateKeys, setDateKeys] = useState("")
+  console.log(NASAData)
 
   // NOT RUNNING
-  // const [dateKeys, setDateKeys] = useState("")
   const [potentialHazardCount, setPotentialHazardCount] = useState("")
   const [nEOToday, setNEOToday] = useState("")
 
 
-  // useEffect(() => {
-  //   const fetchNASAData = () => {
-  //     fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=CQVz4qc29PYDruOh6NPKRzNMtIIYdLMZM589ut8w`)
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         console.log(data)
-  //         setNASAData(data)
-  //         return
-  //       })
-  //   }
-  //   fetchNASAData()
-  // },[])
+  useEffect(() => {
+    const fetchNASAData = () => {
+      fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=CQVz4qc29PYDruOh6NPKRzNMtIIYdLMZM589ut8w`)
+        .then(response => response.json())
+        .then(data => {
+          setNASAData(data)
+          setDateKeys(Object.keys(data.near_earth_objects))
+
+          console.log(data)
+          console.log(dateKeys)
+          return
+        })
+    }
+    fetchNASAData()
+  },[])
 
 
 
@@ -47,18 +52,17 @@ const App = () => {
 
 
 // WORKING
-let dateKeys
+let displayText = "howdy"
 if (!NASAData){
   console.log("loading")
 } else {
-  dateKeys = Object.keys(NASAData.near_earth_objects)
-  console.log(dateKeys)
-  console.log("data retrieved")
+  displayText = NASAData.near_earth_objects[startDate].length
+  console.log("keys retrieved")
 }
 
     return (
       <div>
-        HELLO, EARTHLINGS! TODAY IS {userFormattedDate}. So far this week, Earth has encountered {NASAData.element_count} near-Earth objects (NEOs). So far today, you have avoided {NASAData.near_earth_objects[startDate].length} near earth objects!
+        HELLO, EARTHLINGS! TODAY IS {userFormattedDate}. So far this week, Earth has encountered {NASAData.element_count} near-Earth objects (NEOs). So far today, you have avoided {displayText} near earth objects!
       
       </div>
     )
@@ -69,7 +73,7 @@ export default App;
   //NASA
   // const nasaKey = CQVz4qc29PYDruOh6NPKRzNMtIIYdLMZM589ut8w
   // const nasaURL = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=DEMO_KEY`
-
+  // {!NASAData.near_earth_objects[startDate].length ? "loading" : NASAData.near_earth_objects[startDate].length} 
   //TWITTER
   // const twitterKey = "hkFsJo3avUMCr2EgVlstYk8Ae"
   // const twitterSecretKey = "rKP63FL4ONxSgVpU1lC8kNYLrG6c9kCCOm6CUAiahGoljNq9n9"
---
+++ src/quotes.js
@@ -0,0 +1,86 @@
+const quotes = [{
+    quote: "The Universe is under no obligation to make sense to you.",
+    author: "Neil deGrasse Tyson"
+},
+{
+    quote: "There are no passengers on spaceship earth. We are all crew.",
+    author: "Marshall McLuhan"
+},
+{
+    quote: "I would like to die on Mars. Just not on impact.",
+    author: "Elon Musk"
+},
+{
+    quote: "The moon is a friend for the lonesome to talk to.",
+    author: "Carl Sagan"
+},
+{
+    quote: "The Earth is the cradle of humanity, but mankind cannot stay in the cradle forever.",
+    author: "Konstantin Tsiolkovsky"
+},
+{
+    quote: "The stars don't look bigger, but they do look brighter.",
+    author: "Sally Ride"
+},
+{
+    quote: "Space is to place as eternity is to time.",
+    author: "Joseph Joubert"
+},
+{
+    quote: "If the Sun and the Moon should ever doubt, they'd immediately go out.",
+    author: "William Blake"
+},
+{
+    quote: "I could have gone on flying through space forever.",
+    author: "Yuri Gagarin"
+},
+{
+    quote: "If the Earth gets hit by an asteroid, it's game over. It's control-alt-delete for civilization.",
+    author: "Bill Nye"
+},
+{
+    quote: "Houston, we've had a problem.",
+    author: "Jim Lovell"
+},
+{
+    quote: "The eternal silence of these infinite spaces frightens me.",
+    author: "William Blake"
+},
+{
+    quote: "I see Earth! It is so beautiful.",
+    author: "Yuri Gagarin"
+},
+{
+    quote: "Only dark listening to dark.",
+    author: "Carl Sandburg"
+},
+{
+    quote: "If we can think this far, might not our eyes adjust to the dark?",
+    author: "Sarah Howe"
+}]
+
+export default quotes
+
+// On Foot I Had to Cross the Solar System
+// On foot
+// I had to cross the solar system
+// before I found the first thread of my red dress.
+// I sense myself already.
+// Somewhere in space hangs my heart,
+// shaking in the void, from it stream sparks
+// into other intemperate hearts. 
+
+// Edith Sodergran
+
+// Evening
+
+// The chimneys, rank on rank,
+// Cut the clear sky;
+// The moon,
+// With a rag of gauze about her loins
+// Poses among them, an awkward Venus —
+
+// And here am I looking wantonly at her
+// Over the kitchen sink.
+
+// Richard Aldington
