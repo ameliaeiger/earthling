dee4d3a3c89d9785b4f8278d9c5efda54971ad5b
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-08-04 14:44:15 -0600

    Separate components into files

--- src/App.js
+++ src/App.js
@@ -2,39 +2,35 @@ import './App.css'
 import React, { useEffect, useState } from "react"
 import dayjs from 'dayjs'
 // import data from "./data.js"
-import quotes from "./quotes"
+// import quotes from "./quotes"
+import NEO from "./components/NEO"
+import POD from "./components/POD"
+import { fetchNASAData, fetchAPOD } from "./apiCalls"
 
 const App = () => {
-  console.log(quotes)
   const date = dayjs().format("YYYY-MM-DD")
+  const [loading, setLoading] = useState(true)
   const [userFormattedDate, setUserFormattedDate] = useState(dayjs(date).format("MM/DD/YYYY"))
   const [startDate, setStartDate] = useState(date)
   const [endDate, setEndDate] = useState(dayjs(date).subtract(6, "day").format("YYYY-MM-DD"))
 
 // NASA DATA & OBJECT KEYS
+  const [apodData, setApodData] = useState("")
   const [NASAData, setNASAData] = useState("")
   const [dateKeys, setDateKeys] = useState("")
   console.log(NASAData)
 
-  // NOT RUNNING
-  const [potentialHazardCount, setPotentialHazardCount] = useState("")
-  const [nEOToday, setNEOToday] = useState("")
-
-
   useEffect(() => {
-    const fetchNASAData = () => {
-      fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=CQVz4qc29PYDruOh6NPKRzNMtIIYdLMZM589ut8w`)
-        .then(response => response.json())
-        .then(data => {
-          setNASAData(data)
-          setDateKeys(Object.keys(data.near_earth_objects))
-
-          console.log(data)
-          console.log(dateKeys)
-          return
-        })
-    }
-    fetchNASAData()
+    fetchNASAData(startDate, endDate).then(data => {
+      setNASAData(data)
+      setDateKeys(Object.keys(data.near_earth_objects))
+    })
+    fetchAPOD().then(data => {
+      console.log(data)
+      setApodData(data)
+      console.log(apodData)
+      setLoading(false)
+    })
   },[])
 
 
@@ -51,24 +47,20 @@ const App = () => {
   // }
 
 
-// WORKING
-let displayText = "howdy"
-if (!NASAData){
-  console.log("loading")
-} else {
-  displayText = NASAData.near_earth_objects[startDate].length
-  console.log("keys retrieved")
-}
-
     return (
       <div>
-        HELLO, EARTHLINGS! TODAY IS {userFormattedDate}. So far this week, Earth has encountered {NASAData.element_count} near-Earth objects (NEOs). So far today, you have avoided {displayText} near earth objects!
-      
+        <NEO data={NASAData} date={userFormattedDate} startDate={startDate} />
+        <POD apodData={apodData} />      
       </div>
     )
 }
 
 export default App;
+
+
+
+
+
 
   //NASA
   // const nasaKey = CQVz4qc29PYDruOh6NPKRzNMtIIYdLMZM589ut8w
@@ -79,10 +71,3 @@ export default App;
   // const twitterSecretKey = "rKP63FL4ONxSgVpU1lC8kNYLrG6c9kCCOm6CUAiahGoljNq9n9"
   // const twitterBearerToken = "AAAAAAAAAAAAAAAAAAAAANUjfgEAAAAAYRYeLe%2FO%2BlGtihaAYT%2BhTapVbAU%3DmNKxQL15TRVtiuvOGqungPQfglPuOkdQYhOppHaLcG0FVZRBa6"
   // let text = `Today there are ${}`
-
-
-  // I want to access NASA data's near earth objects object.
-  // I want to iterate through the past week's arrays (7) of near earth objects.
-  // From there, I will 
-  // For each occurrence of a near earth space object, I will access the object's "is_potentially_hazardous_asteroid" data.
-  // 
--- src/components/NEO.js
+++ src/components/NEO.js
@@ -1,11 +1,48 @@
 import React, { useEffect, useState } from "react"
-import dayjs from 'dayjs'
-import quotes from "./quotes"
+import dayjs from "dayjs"
+
+let numHazardous
+const NEO = ({ date, data, startDate, apodData }) => {
+  // NOT RUNNING
+//   const [numHazardous, setNumHazardous] = useState("")
+  const [nEOToday, setNEOToday] = useState("")
 
 
-const NEO = ({ date, data, displayText }) => {
+const getData = (data) => {
+    console.log(data.near_earth_objects[startDate])
+    let result = []
+    data.near_earth_objects[startDate].forEach(data => {
+      result.push(data.is_potentially_hazardous_asteroid)
+    })
+    let count = result.filter(boolean => boolean)
+    console.log(result)
+    console.log(count.length)
+    numHazardous = count.length
+  }
+  
+      // WORKING
+let displayText = "howdy"
+if (!data){
+  console.log("loading")
+} else {
+  displayText = data.near_earth_objects[startDate].length
+  getData(data)
+
+
+}
+  
+
+
     return (
-        `HELLO, EARTHLINGS! TODAY IS ${date}. So far this week, Earth has encountered ${data.element_count} near-Earth objects (NEOs). So far today, you have avoided ${displayText} near earth objects!`
+        <div>
+        `HELLO, EARTHLINGS! TODAY IS {date}. 
+        So far this week, Earth has encountered {data.element_count} 
+        near-Earth objects (NEOs). So far today, you have encountered {displayText} near earth objects, and avoided 
+        {numHazardous} potentially hazardous situations.`        
+        </div>
+
+
+        
     )
 }
 
