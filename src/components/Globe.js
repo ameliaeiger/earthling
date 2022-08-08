babba18d78128e60580c5b8c9200d854eae20995
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-08-07 20:38:43 -0600

    Fix Globe size issue

--- src/components/Globe.js
+++ src/components/Globe.js
@@ -1,55 +1,35 @@
-import React from "react";
-import { useEffect, useState, useRef, useCallback} from "react";
-import Button from "@mui/material/Button"
-
-import Globe from "react-globe.gl";
+import React from "react"
+import { useEffect, useState, useRef } from "react"
+import Globe from "react-globe.gl"
 import globeImage from "../assets/earth-large.jpg"
 
 export default function CustomGlobe() {
     const globeEl = useRef()
-    const [rotation, setRotation] = useState(true);
-    const [countries, setCountries] = useState({ features: [] });
-
-
+    const [rotation, setRotation] = useState(true)
 
     useEffect(() => {
-        fetch(
-          "https://raw.githubusercontent.com/vasturiano/react-globe.gl/master/example/datasets/ne_110m_admin_0_countries.geojson"
-        )
-          .then((res) => res.json())
-          .then((countries) => {
-            setCountries(countries);
-          });
-      }, []);
-
-    useEffect(() => {
-        globeEl.current.pointOfView({ altitude: 3 }, 5000);
+        globeEl.current.pointOfView({ altitude: 2 }, 5000)
         globeEl.current.controls().autoRotate = true;
-        globeEl.current.controls().autoRotateSpeed = 2;
-              }, [rotation]);
+        globeEl.current.controls().autoRotateSpeed = 2
+    }, [rotation]);
 
   return (
     <section style={styles.globeWrapper}>
-      <div style={styles.globeDiv}>
     <Globe
         ref={globeEl}
         globeImageUrl={globeImage}
         backgroundColor="rgba(0,0,0,0)"
-        hexPolygonResolution={3} //values higher than 3 makes it buggy
+        hexPolygonResolution={3} // no go above 3 or else u a clown
     />
-    </div>
     </section>
-  );
+  )
 }
 
 const styles = {
-    globeDiv: {
-      display:"flex",
-      justifyContents:"center",
-      alignItems:"center",
-    },
     globeWrapper: {
-        height:"100%",
+        position:"fixed",
+        top:30,
+        height:"90%",
         width:"100%",
         justifyContents:"center",
         alignItems:"center",
